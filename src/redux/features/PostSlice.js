95291b15d1d662f9eb6f56282f9023fc54218c12
pankajinventam <pankaj.inventam@gmail.com>
2022-04-04 18:04:33 +0530

    store setup

---
+++ src/redux/features/PostSlice.js
@@ -0,0 +1,31 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const getPost = createAsyncThunk("post/getPosts", async ({ id }) => {
+  return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then((res) =>
+    res.json()
+  );
+});
+
+const PostSlice = createSlice({
+  name: "post",
+  initialState: {
+    loading: false,
+    post: [],
+    error: null,
+  },
+  extraReducers: {
+    [getPost.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [getPost.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.post = [action.payload];
+    },
+    [getPost.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
+});
+
+export default PostSlice
---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import PostSlice from "./features/PostSlice";
+
+export default configureStore({
+  reducer: {
+    app: PostSlice,
+  },
+});
